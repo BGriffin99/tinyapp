c033c8f7deec36c5ff5ca30a9b229e1dd73bbcea
Brady Griffin <bradyklg@gmail.com>
2023-04-18 19:53:58 -0400

    update

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -15,10 +17,42 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
   });
 
   app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
+  });
+
+  app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
+
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
+  app.get("/urls/:id", (req, res) => {
+    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+    res.render("urls_show", templateVars);
+  });
+
+  app.post('/urls/:id/delete', (req, res) => {
+    const id = req.params.id;
+    delete urlDatabase[id];
+    res.redirect('/urls');
+  });
+
+  app.get("/urls/:id/edit", (req, res) => {
+    const templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
+    res.render("urls_show", templateVars);
+  });
+  
+  app.post("/urls/:id/update", (req, res) => {
+    urlDatabase[req.params.id] = req.body.longURL;
+    res.redirect("/urls");
   });
